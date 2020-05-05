3ed685cbf44c7eec80b20377ff74b1b2e2f240e8
olakehinde <olakehinde59@gmail.com>
2020-05-05 05:11:00 -0700

    added migrations file for books table

---
+++ database/migrations/2020_05_05_120703_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->bigIncrement('id');
+            $table->string('title');
+            $table->integer('pages', 11);
+            $table->integer('author_id');
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+}
