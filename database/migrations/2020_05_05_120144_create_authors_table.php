598cc907b0fcf0d3846cb0c21202ce97ced11b3d
olakehinde <olakehinde59@gmail.com>
2020-05-05 05:06:29 -0700

    created migration file for authors table

---
+++ database/migrations/2020_05_05_120144_create_authors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->timestamp();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('authors', function (Blueprint $table) {
+            //
+        });
+    }
+}
